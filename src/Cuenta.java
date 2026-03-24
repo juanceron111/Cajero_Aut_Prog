8303a69c69861120781e46b0089a8ebe4bb912c2
Andres Castro <ascastrobz@gmail.com>
2026-03-24 02:27:45 -0500

    Primer avance de la clase Cuenta

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -19,4 +19,34 @@ public class Cuenta {
         this.saldo = saldo;
     }
 
+    public Cuenta(long datosTitular, double saldo) {
+        this.datosTitular = datosTitular;
+        this.saldo = saldo;
+    }
+
+    public boolean retirar(int monto) {
+        if (monto > LIMITE_DIARIO) {
+            System.out.println("El monto excede el límite diario.");
+            return false;
+        }
+        if (monto > saldo) {
+            System.out.println("Fondos insuficientes.");
+            return false;
+        }
+        saldo -= monto;
+        return true;
+    }
+
+    public boolean consignar(int monto) {
+        if (monto > LIMITE_DIARIO) {
+            System.out.println("El monto excede el límite diario.");
+            return false;
+        }
+        saldo += monto;
+        return true;
+    }
+
+    public long verSaldo() {
+        return (long) saldo;
+    }
 }
